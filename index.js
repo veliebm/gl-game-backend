010f561d480118bf74dc7c3e75dfa894a8d97467
Benjamin Velie <veliebm@google.com>
2023-07-19 18:00:08 -0500

    Rename files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Run this script to start the server.
  */
 
-const { startServer } = require("./server");
+const { startServer } = require("./http-server");
 
 const port = process.env.PORT || 8000;
 const hostname = process.env.HOSTNAME || "127.0.0.1";
