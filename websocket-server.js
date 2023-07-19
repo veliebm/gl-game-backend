f7e01866873c1bdc836a7d5e410225097a2bbd06
Benjamin Velie <veliebm@google.com>
2023-07-19 18:32:42 -0500

    Break onMessage into its own function

--- websocket-server.js
+++ websocket-server.js
@@ -9,6 +9,29 @@ const activeConnections = {};
 /** The WebSocket Server. */
 const webSocketServer = new websocket.server({ httpServer });
 
+/** When a message comes in, forward it along. */
+const _onMessage = (data, clientId) => {
+  if (data.type !== "utf8") {
+    console.error(
+      `Client ${clientId} tried sending data with invalid type: ${data.type}`
+    );
+  }
+
+  console.log(`Incoming message from Client ${clientId}: ${data.utf8Data}`);
+  const message = JSON.parse(data.utf8Data);
+  const recipientId = message.id;
+  const recipient = activeConnections[recipientId];
+
+  if (recipient) {
+    message.id = clientId;
+    const outgoingData = JSON.stringify(message);
+    console.log(`Sending to Client ${recipientId}: ${outgoingData}`);
+    recipient.send(outgoingData);
+  } else {
+    console.error(`Recipient Client ${recipientId} not found`);
+  }
+};
+
 /** When a client disconnects, delete them from active connections. */
 const _onClose = (clientId) => {
   delete activeConnections[clientId];
@@ -21,30 +44,7 @@ const _onRequest = (request) => {
   const connection = request.accept(null, request.origin);
   console.log(`New client has connected: ${clientId}`);
 
-  // When a client sends a message, forward it to its recipient.
-  connection.on("message", (data) => {
-    if (data.type !== "utf8") {
-      console.error(
-        `Client ${clientId} tried sending data with invalid type: ${data.type}`
-      );
-    }
-
-    console.log(`Incoming message from Client ${clientId}: ${data.utf8Data}`);
-    const message = JSON.parse(data.utf8Data);
-    const recipientId = message.id;
-    const recipient = activeConnections[recipientId];
-
-    if (recipient) {
-      message.id = clientId;
-      const outgoingData = JSON.stringify(message);
-      console.log(`Sending to Client ${recipientId}: ${outgoingData}`);
-      recipient.send(outgoingData);
-    } else {
-      console.error(`Recipient Client ${recipientId} not found`);
-    }
-  });
-
-  // When a client disconnects, remove them from the server's active connections.
+  connection.on("message", (data) => _onMessage(data, clientId));
   connection.on("close", () => _onClose(clientId));
 
   activeConnections[clientId] = connection;
